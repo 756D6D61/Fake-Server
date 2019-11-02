5ed9bb1bc68763cfaf1e0f36bbc0bb6d77c9a214
756D6D61 <756D6D61@gmail.com>
2019-11-02 16:29:49 +0000

    semi-working

--- routes/index.js
+++ routes/index.js
@@ -1,79 +1,89 @@
-var express = require('express')
-var { format } = require('date-fns')
-const random = require('canvas-sketch-util/random')
-var router = express.Router()
+var express = require('express');
+var { format } = require('date-fns');
+const random = require('canvas-sketch-util/random');
+var router = express.Router();
 
-const getValue = (preValue, boundaries) => {
-  if(!prevValue) {
-    return random.range(boundaries[0], boundaries[1])
+const getValue = (prevValue, boundaries) => {
+  if (!prevValue) {
+    // Get first value with random number between the two boundaries
+    return random.range(boundaries[0], boundaries[1]);
   } else {
-    const increment = random.range(boundaries[0], boundaries[1]) * 0.01 * (random.value() > 0.5 ? -1:1)
-    return preValue + (preValue + increment < 0 ? increment * -1 : increment)
+    const increment =
+      random.range(boundaries[0], boundaries[1]) * 0.05 * random.sign();
+
+    let newValue =
+      prevValue + (prevValue + increment < 0 ? increment * -1 : increment);
+    return newValue;
   }
-}
+};
 
 const routes = [
-  { name: 'activations', boundaries: [1000, 2000] }, 
-  { name: 'on-time', boundaries: [0.1, 1] }
-]
+  {
+    name: 'savings',
+    boundaries: [0, 10000]
+  }
+];
 
 const splitByConfig = {
-  technology: ['adsl', 'fttc']
-}
+  month: [
+    'jan',
+    'feb',
+    'march',
+    'apr',
+    'may',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dev'
+  ],
+  cateogry: ['birthday', 'wedding', 'holiday', 'misc'],
+  undefined: []
+};
 
 routes.map(route => {
   return router.get(`/${route.name}`, function(req, res, next) {
-    let data = {}
-    const splitBy = req['split-by']
-    const start = req.query['start']
-    const end = req.query['end']
+    let data = {};
+    // Get query params from URL
+    const splitBy = req.query['split-by'];
+    const start = req.query['start'];
+    const end = req.query['end'];
 
-    const splitBys = splitByConfig[splitBy] || []
+    const splitBys = splitByConfig[splitBy] || [];
 
-    let allTimeStamps = []
+    let allTimestamps = [];
 
-    if(start && end) {
-      const startTimeStamp = Number(format(new Date(start), 'T'))
-      const endTimeStamp = Number(format(new Date(end), 'T'))
+    if (start && end) {
+      // Convert UNIX style date (2019-10-24T13:40:00Z) to timestamp, and then convert to a number so that we can add to it later
+      const startTimestamp = Number(format(new Date(start), 'T'));
+      const endTimestamp = Number(format(new Date(end), 'T'));
 
-      for (let i = startTimeStamp; i<= endTimeStamp; i = i+90000) {
-        allTimeStamps.push(i)
+      // Increment 900000 milliseconds (15 minutes) from the startTimestamp until the endTimestamp
+      for (let i = startTimestamp; i <= endTimestamp; i = i + 900000) {
+        allTimestamps.push(i);
       }
     }
 
     splitBys.forEach(split => {
-      let values = []
-      return (data[split] = allTimeStamps.map((timestamp, index) => {
-        const prevValue = values.length > 0 ? values[index - 1] : false
-        const value = getValue(prevValue, route.boundaries)
-        values.push(value)
+      let values = [];
+      return (data[split] = allTimestamps.map((timestamp, index) => {
+        const prevValue = values.length > 0 ? values[index - 1] : false;
+        const value = getValue(prevValue, route.boundaries);
+        values.push(value);
         return {
-          timestamp, 
+          timestamp,
           value
-        }
-      }))
-    })
+        };
+      }));
+    });
 
-    let total = []
-    Object.keys(data).map(dataKey => {
-      return data[dataKey].map((datum, index) => {
-        if(total[index]) {
-          const summedValue= {
-            ...total[index],
-            value: total[index].value + datum.value
-          }
-          return total.splice(index, 1, summedValue)
-        } else {
-          return total.push(datum)
-        }
-      })
-    })
-    data. total = total
     res.json({
       meta: {},
       data
-    })
-  })
-})
+    });
+  });
+});
 
 module.exports = router;
